e07e8dc3a7fd5b4dc54c17d1e793989259399b31
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2024-10-01 16:44:00 -0700

    Discover peers

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"slices"
 	"strconv"
@@ -156,17 +161,19 @@ func runDecodeCommand(value string) error {
 	return nil
 }
 
-func runInfoCommand(file string) error {
-	payload, err := os.ReadFile(file)
+type TorrentInfo struct {
+	trackerURL  string
+	length      int
+	hash        []byte
+	pieceLength int
+	pieces      [][]byte
+}
+
+func getInfo(payload string) (*TorrentInfo, error) {
+	decodedValue, _, err := decodeBencode(payload)
 
 	if err != nil {
-		return err
-	}
-
-	decodedValue, _, err := decodeBencode(string(payload))
-
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	dict := decodedValue.(map[string]interface{})
@@ -174,22 +181,139 @@ func runInfoCommand(file string) error {
 	h := sha1.New()
 
 	if _, err := h.Write([]byte(encode(info))); err != nil {
+		return nil, err
+	}
+
+	rawPieces := info["pieces"].(string)
+	var pieces [][]byte
+	for i := 0; i < len(rawPieces); i += 20 {
+		pieces = append(pieces, []byte(rawPieces[i:i+20]))
+	}
+
+	return &TorrentInfo{
+		trackerURL:  dict["announce"].(string),
+		length:      info["length"].(int),
+		hash:        h.Sum(nil),
+		pieceLength: info["piece length"].(int),
+		pieces:      pieces,
+	}, nil
+}
+
+func runInfoCommand(file string) error {
+	payload, err := os.ReadFile(file)
+
+	if err != nil {
 		return err
 	}
 
-	infoHash := hex.EncodeToString(h.Sum(nil))
+	info, err := getInfo(string(payload))
 
-	fmt.Printf("Tracker URL: %s\n", dict["announce"])
-	fmt.Printf("Length: %d\n", info["length"])
-	fmt.Printf("Info Hash: %s\n", infoHash)
-	fmt.Printf("Piece Length: %d\n", info["piece length"])
-	fmt.Println("Piece Hashes:")
-
-	pieces := info["pieces"].(string)
-
-	for i := 0; i < len(pieces); i += 20 {
-		fmt.Println(hex.EncodeToString([]byte(pieces[i : i+20])))
+	if err != nil {
+		return err
 	}
+
+	fmt.Printf("Tracker URL: %s\n", info.trackerURL)
+	fmt.Printf("Length: %d\n", info.length)
+	fmt.Printf("Info Hash: %s\n", hex.EncodeToString(info.hash))
+	fmt.Printf("Piece Length: %d\n", info.pieceLength)
+	fmt.Println("Piece Hashes:")
+	for _, piece := range info.pieces {
+		fmt.Println(hex.EncodeToString(piece))
+	}
+
+	return nil
+}
+
+func runPeersCommand(file string) error {
+	payload, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("could not read file in peers command: %w", err)
+	}
+
+	info, err := getInfo(string(payload))
+
+	if err != nil {
+		return err
+	}
+
+	u, err := url.Parse(info.trackerURL)
+	if err != nil {
+		return fmt.Errorf("could not parse URL in peers command: %w", err)
+	}
+
+	query := u.Query()
+	query.Add("info_hash", string(info.hash))
+	query.Add("peer_id", "00112233445566778899")
+	query.Add("port", "6881")
+	query.Add("uploaded", "0")
+	query.Add("downloaded", "0")
+	query.Add("left", strconv.Itoa(info.length))
+	query.Add("compact", "1")
+
+	u.RawQuery = query.Encode()
+
+	r, err := http.Get(u.String())
+	if err != nil {
+		return fmt.Errorf("could not make HTTP request in peers command: %w", err)
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("could not read HTTP response in peers command: %w", err)
+	}
+	defer r.Body.Close()
+
+	parsedResp, _, err := decodeBencode(string(body))
+	if err != nil {
+		return err
+	}
+
+	m := parsedResp.(map[string]interface{})
+	peers := []byte(m["peers"].(string))
+
+	for i := 0; i < len(peers); i += 6 {
+		peer := peers[i : i+6]
+		ip := fmt.Sprintf("%d.%d.%d.%d", peer[0], peer[1], peer[2], peer[3])
+		port := binary.BigEndian.Uint16(peer[4:])
+		fmt.Printf("%s:%d\n", ip, port)
+	}
+
+	return nil
+}
+
+func runHandshakeCommand(file, peer string) error {
+	payload, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("could not read file in peers command: %w", err)
+	}
+
+	info, err := getInfo(string(payload))
+
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.Dial("tcp", peer)
+	if err != nil {
+		return err
+	}
+
+	message := []byte{19}
+	message = append(message, []byte("BitTorrent protocol")...)
+	message = append(message, []byte{0, 0, 0, 0, 0, 0, 0, 0}...)
+	message = append(message, info.hash...)
+	message = append(message, []byte("00112233445566778899")...)
+
+	if _, err := conn.Write(message); err != nil {
+		return fmt.Errorf("error sending message to peer: %w", err)
+	}
+
+	var resp []byte
+	if _, err := conn.Read(resp); err != nil {
+		return fmt.Errorf("error reading message from peer: %w", err)
+	}
+
+	fmt.Println(string(resp[48:68]))
 
 	return nil
 }
@@ -205,6 +329,17 @@ func main() {
 		}
 	case "info":
 		if err := runInfoCommand(os.Args[2]); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	case "peers":
+		if err := runPeersCommand(os.Args[2]); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+	case "handshake":
+		if err := runHandshakeCommand(os.Args[2], os.Args[3]); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
