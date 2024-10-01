a9fb63190dcccfae20c7d92ba5a77de423966204
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2024-10-01 12:23:57 -0700

    Decode bencoded integers

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -16,27 +16,53 @@ var _ = json.Marshal
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
+	switch {
+	case unicode.IsDigit(rune(bencodedString[0])):
+		return decodeString(bencodedString)
+	case bencodedString[0] == 'i':
+		return decodeInteger(bencodedString)
+	default:
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
+}
+
+func decodeInteger(bencodedString string) (interface{}, error) {
+	var rawInteger []byte
+
+	for i := 1; i < len(bencodedString); i++ {
+		if (i == 1 && bencodedString[i] == '-') || unicode.IsDigit(rune(bencodedString[i])) {
+			rawInteger = append(rawInteger, bencodedString[i])
+			continue
+		}
+
+		if bencodedString[i] == 'e' {
+			return strconv.Atoi(string(rawInteger))
+		}
+
+		return nil, fmt.Errorf("invalid integer")
+	}
+
+	return nil, fmt.Errorf("invalid integer")
+}
+
+func decodeString(bencodedString string) (interface{}, error) {
+	var firstColonIndex int
+
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bencodedString[:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", err
+	}
+
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
 }
 
 func main() {
