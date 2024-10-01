04124ab96e96ee11f055a1c6b5369f3d030b5c19
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2024-10-01 14:58:08 -0700

    Parse torrent file

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
@@ -16,33 +15,33 @@ var _ = json.Marshal
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, int, error) {
+func decodeBencode(value []byte) (interface{}, int, error) {
 	switch {
-	case unicode.IsDigit(rune(bencodedString[0])):
-		return decodeString(bencodedString)
-	case bencodedString[0] == 'i':
-		return decodeInteger(bencodedString)
-	case bencodedString[0] == 'l':
-		return decodeList(bencodedString)
-	case bencodedString[0] == 'd':
-		return decodeDictionary(bencodedString)
+	case unicode.IsDigit(rune(value[0])):
+		return decodeString(value)
+	case value[0] == 'i':
+		return decodeInteger(value)
+	case value[0] == 'l':
+		return decodeList(value)
+	case value[0] == 'd':
+		return decodeDictionary(value)
 	default:
 		return "", 0, fmt.Errorf("Invalid payload")
 	}
 }
 
-func decodeDictionary(bencodedString string) (interface{}, int, error) {
+func decodeDictionary(value []byte) (interface{}, int, error) {
 	dict := map[string]interface{}{}
 	index := 1
 	var key string
 	var parsedValues int
 
-	for index < len(bencodedString) {
-		if bencodedString[index] == 'e' {
+	for index < len(value) {
+		if value[index] == 'e' {
 			return dict, index + 1, nil
 		}
 
-		r, length, err := decodeBencode(bencodedString[index:])
+		r, length, err := decodeBencode(value[index:])
 
 		if err != nil {
 			return nil, 0, err
@@ -63,16 +62,16 @@ func decodeDictionary(bencodedString string) (interface{}, int, error) {
 	return nil, 0, fmt.Errorf("invalid dictionary")
 }
 
-func decodeList(bencodedString string) (interface{}, int, error) {
+func decodeList(value []byte) (interface{}, int, error) {
 	list := []interface{}{}
 	index := 1
 
-	for index < len(bencodedString) {
-		if bencodedString[index] == 'e' {
+	for index < len(value) {
+		if value[index] == 'e' {
 			return list, index + 1, nil
 		}
 
-		result, length, err := decodeBencode(bencodedString[index:])
+		result, length, err := decodeBencode(value[index:])
 		if err != nil {
 			return nil, 0, err
 		}
@@ -83,12 +82,12 @@ func decodeList(bencodedString string) (interface{}, int, error) {
 	return nil, 0, fmt.Errorf("invalid list")
 }
 
-func decodeInteger(bencodedString string) (interface{}, int, error) {
-	end := strings.Index(bencodedString, "e")
+func decodeInteger(value []byte) (interface{}, int, error) {
+	end := bytes.IndexRune(value, 'e')
 	if end == -1 {
 		return nil, 0, fmt.Errorf("invalid integer")
 	}
-	integer, err := strconv.Atoi(bencodedString[1:end])
+	integer, err := strconv.Atoi(string(value[1:end]))
 	if err != nil {
 		return nil, 0, fmt.Errorf("invalid integer: %w", err)
 	}
@@ -96,33 +95,64 @@ func decodeInteger(bencodedString string) (interface{}, int, error) {
 	return integer, end + 1, nil
 }
 
-func decodeString(bencodedString string) (interface{}, int, error) {
-	firstColonIndex := strings.Index(bencodedString, ":")
-	lengthStr := bencodedString[:firstColonIndex]
+func decodeString(value []byte) (interface{}, int, error) {
+	firstColonIndex := bytes.IndexRune(value, ':')
+	lengthStr := value[:firstColonIndex]
 
-	length, err := strconv.Atoi(lengthStr)
+	length, err := strconv.Atoi(string(lengthStr))
 	if err != nil {
 		return "", 0, err
 	}
 
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], len(lengthStr) + 1 + length, nil
+	return string(value[firstColonIndex+1 : firstColonIndex+1+length]), len(lengthStr) + 1 + length, nil
+}
+
+func runDecodeCommand(value string) error {
+	decoded, _, err := decodeBencode([]byte(value))
+	if err != nil {
+		return err
+	}
+
+	jsonOutput, _ := json.Marshal(decoded)
+	fmt.Println(string(jsonOutput))
+	return nil
+}
+
+func runInfoCommand(file string) error {
+	payload, err := os.ReadFile(file)
+
+	if err != nil {
+		return err
+	}
+
+	decodedValue, _, err := decodeBencode(payload)
+
+	if err != nil {
+		return err
+	}
+
+	dict := decodedValue.(map[string]interface{})
+	info := dict["info"].(map[string]interface{})
+
+	fmt.Print("Tracker URL: ", dict["announce"], "Length: ", info["length"])
+	return nil
 }
 
 func main() {
 	command := os.Args[1]
 
-	if command == "decode" {
-		bencodedValue := os.Args[2]
-
-		decoded, _, err := decodeBencode(bencodedValue)
-		if err != nil {
+	switch command {
+	case "decode":
+		if err := runDecodeCommand(os.Args[2]); err != nil {
 			fmt.Println(err)
-			return
+			os.Exit(1)
 		}
-
-		jsonOutput, _ := json.Marshal(decoded)
-		fmt.Println(string(jsonOutput))
-	} else {
+	case "info":
+		if err := runInfoCommand(os.Args[2]); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
 	}
